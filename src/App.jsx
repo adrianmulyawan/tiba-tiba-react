cb98df04a0b114ab7049d18180f13e5d48352aef
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 13:28:14 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
           <div className="col-lg-5">
             <div className="login-header">
               <h1 className="text-primary">Login</h1>
@@ -15,7 +15,7 @@ function App() {
             <form method="#">
               <Input label="email" text="Email" type="email" placeholder="Enter your email..." id="email" />
               <Input label="password" text="Password" type="password" placeholder="Enter your password..." id="password" />
-              <div class="d-grid gap-2">
+              <div className="d-grid gap-2">
                 <Button buttonType="submit" buttonStyle="btn-primary rounded-4">Login</Button>
               </div>
             </form>
