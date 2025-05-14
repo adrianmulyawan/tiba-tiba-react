bc216ea9315108816f150178ca34799ec7b258ff
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 14:24:49 +0700

    edit form login and register

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -10,6 +10,7 @@ const FormLogin = () => {
         <Input label="password" text="Password" type="password" placeholder="Enter your password..." id="password" />
         <div className="d-grid gap-2">
           <Button buttonType="submit" buttonStyle="btn-primary rounded-4">Login</Button>
+          <p className="text-center text-primary my-2">Belum punya akun? <a className="fw-bold" href="/register">daftar disini</a></p>
         </div>
       </form>
     </>
--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -13,6 +13,7 @@ const FormRegister = () => {
         <Input label="phone_number" text="Phone Number" type="text" placeholder="Enter your phone number..." id="phone_number" />
         <div className="d-grid gap-2">
           <Button buttonType="submit" buttonStyle="btn-primary rounded-4">Register</Button>
+          <p className="text-center text-primary my-2">Sudah punya akun? <a href="/login" className="fw-bold">klik disini</a></p>
         </div>
       </form>
     </>
