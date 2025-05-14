b57d55baa72dda885425081f06f4c12a0587f9bf
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 16:54:43 +0700

    implements conditional rendering

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useLocation, Link } from "react-router";
 
 const AuthLayout = (props) => {
   const { children, title } = props;
+  const location = useLocation();
   return (
     <div className="container">
       <div className="d-flex flex-column justify-content-center align-items-center vh-100">
@@ -11,6 +13,20 @@ const AuthLayout = (props) => {
             <p className="fw-lighter">Welcome, please enter your detail!</p>
           </div>
           { children }
+          {/* conditional rendering */}
+          <p className="text-center text-primary my-2">
+            {
+              location.pathname === '/register' ? (
+                <>
+                  Sudah punya akun? <Link to="/login" className="fw-bold">klik disini</Link>
+                </>
+              ) : (
+                <>
+                  Belum punya akun? <Link className="fw-bold" to="/register">daftar disini</Link>
+                </>
+              )
+            }
+          </p>
         </div>
       </div>
     </div>
