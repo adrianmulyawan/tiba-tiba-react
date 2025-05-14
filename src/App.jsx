9a7645b131ae2e12b54ebd33ae4d216a6e1eb032
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 13:46:49 +0700

    call login in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import Button from "./components/Elements/Button";
-import Input from "./components/Elements/Input";
+import LoginPage from "./Pages/login";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-          <div className="col-lg-5">
-            <div className="login-header">
-              <h1 className="text-primary">Login</h1>
-              <p className="fw-lighter">Welcome, please enter your detail!</p>
-            </div>
-            <form method="#">
-              <Input label="email" text="Email" type="email" placeholder="Enter your email..." id="email" />
-              <Input label="password" text="Password" type="password" placeholder="Enter your password..." id="password" />
-              <div className="d-grid gap-2">
-                <Button buttonType="submit" buttonStyle="btn-primary rounded-4">Login</Button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <LoginPage/>
     </>
-
-  )
+  );
 }
 
 export default App
