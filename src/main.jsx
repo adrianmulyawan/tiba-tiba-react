a44a11093ebab92358e5a4e5f22f5d05f2952904
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 14:25:35 +0700

    implements react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.jsx'
+import LoginPage from './Pages/login.jsx';
+import RegisterPage from './Pages/register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path='/' element={<App/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
