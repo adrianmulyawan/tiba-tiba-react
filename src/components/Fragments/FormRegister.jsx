eeed52b8245bb236b9c2fc86956130fc62e7bb6e
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 13:52:48 +0700

    create form register

---
+++ src/components/Fragments/FormRegister.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Input from "../Elements/Input";
+import Button from "../Elements/Button";
+
+const FormRegister = () => {
+  return (
+    <>
+      <form method="POST">
+        <Input label="name" text="Fullname" type="text" placeholder="Enter your name..." id="name" />
+        <Input label="email" text="Email" type="email" placeholder="Enter your email..." id="email" />
+        <Input label="password" text="Password" type="password" placeholder="Enter your password..." id="password" />
+        <Input label="Address" text="Address" type="text" placeholder="Enter your address..." id="address" />
+        <Input label="phone_number" text="Phone Number" type="text" placeholder="Enter your phone number..." id="phone_number" />
+        <div className="d-grid gap-2">
+          <Button buttonType="submit" buttonStyle="btn-primary rounded-4">Register</Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default FormRegister;
