6a844d4c2dec55d6ec0534171e5613041eaa652f
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-05-14 16:34:59 +0700

    add title page

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormLogin from "../components/Fragments/FormLogin";
 
 const LoginPage = () => {
+  useEffect(() => {
+    document.title = 'Login';
+  });
   return (
     <AuthLayout title="Login">
       <FormLogin/>
--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormRegister from "../components/Fragments/FormRegister";
 
 const RegisterPage = () => {
+  useEffect(() => {
+    document.title = 'Register';
+  });
   return (
     <AuthLayout title="Register">
       <FormRegister/>
